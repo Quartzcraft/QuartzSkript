d7f5bf5dde924c9cf71e77888bcd09a63be25bb6
nfelly <nfell2009@outlook.com>
2014-12-03 12:38:01 +0000

    Main.java added

---
+++ src/uk/co/quartzcraft/quartzskript/Main.java
@@ -0,0 +1,12 @@
+package uk.co.quartzcraft.quartzskript;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import ch.njol.skript.Skript;
+
+public class Main extends JavaPlugin {
+	 @Override
+	    public void onEnable() {
+		 //Stuff goes here. Waiting for QuartzCore API info :3
+		}
+}
