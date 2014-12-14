f5602a112e081db2cb81326d8c785097db67ca8e
nfelly <nfell2009@outlook.com>
2014-12-14 17:04:05 +0000

    Adding ChestShop stuff

---
+++ src/uk/co/quartzcraft/quartzskript/ExprPrice.java
@@ -0,0 +1,50 @@
+package uk.co.quartzcraft.quartzskript;
+
+import javax.annotation.Nullable;
+
+import ch.njol.skript.ScriptLoader;
+import ch.njol.skript.Skript;
+import ch.njol.skript.lang.Expression;
+import ch.njol.skript.lang.SkriptParser.ParseResult;
+import ch.njol.skript.lang.util.SimpleExpression;
+import ch.njol.skript.log.ErrorQuality;
+import ch.njol.util.Kleenean;
+import org.bukkit.event.Event;
+
+import com.Acrobot.ChestShop.Events.TransactionEvent;
+
+public class ExprPrice extends SimpleExpression<Number> {
+	 
+    @Override
+    public Class<? extends Number> getReturnType() {
+            return Number.class;
+    }
+
+    @Override
+    public boolean isSingle() {
+            return true;
+    }
+
+    @Override
+    public boolean init(Expression<?>[] e, int i, Kleenean kl, ParseResult pr) {
+            if (!ScriptLoader.isCurrentEvent(TransactionEvent.class)) {
+                    Skript.error(
+                                    "Cannot use client in other events other than the transaction event",
+                                    ErrorQuality.SEMANTIC_ERROR);
+                    return false;
+            }
+            return true;
+    }
+
+    @Override
+    public String toString(@Nullable Event e, boolean b) {
+            return "Returns client";
+    }
+
+    @Override
+    @Nullable
+    protected Number[] get(Event e) {
+            return new Number[] { ((TransactionEvent) e).getPrice() };
+    }
+
+}
--- src/uk/co/quartzcraft/quartzskript/Main.java
+++ src/uk/co/quartzcraft/quartzskript/Main.java
@@ -1,12 +1,30 @@
 package uk.co.quartzcraft.quartzskript;
 
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.Acrobot.ChestShop.Events.TransactionEvent;
+
 import ch.njol.skript.Skript;
+import ch.njol.skript.lang.ExpressionType;
+import ch.njol.skript.lang.util.SimpleEvent;
+import ch.njol.skript.registrations.EventValues;
+import ch.njol.skript.util.Getter;
 
 public class Main extends JavaPlugin {
 	 @Override
 	    public void onEnable() {
-		 //Stuff goes here. Waiting for QuartzCore API info :3
+		 Skript.registerEvent("On ChestShop Transaction", SimpleEvent.class, TransactionEvent.class, "chestshop transaction");
+		 Skript.registerExpression(ExprClient.class, Player.class, ExpressionType.SIMPLE, "client");
+		 Skript.registerExpression(ExprPrice.class, Number.class, ExpressionType.SIMPLE, "price");
+		 
+		 EventValues.registerEventValue(TransactionEvent.class,
+			        Player.class, new Getter<Player, TransactionEvent>() {
+			                @Override
+			                @javax.annotation.Nullable
+			                public Player get(TransactionEvent e) {
+			                        return (Player) e.getClient();
+			                }
+		 			}, 0);
 		}
 }
